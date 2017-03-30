f4296e6e8caa950b9edc7a007217837a42dbadf1
Zak Auerbach <zauerbach@pivotal.io>
2017-03-30 11:07:25 -0700

    Use m3 until a new release is cut
    we need a code change in the cpi to support i3 instance storage. This
    pipeline will be red if we try to deploy i3s so we must wait.

--- aws/tasks/run-e2e.sh
+++ aws/tasks/run-e2e.sh
@@ -63,7 +63,7 @@ vm_types:
       availability_zone: ${AVAILABILITY_ZONE}
   - name: raw_ephemeral_pool
     cloud_properties:
-      instance_type: i3.large
+      instance_type: m3.medium
       availability_zone: ${AVAILABILITY_ZONE}
       raw_instance_storage: true
   - name: elb_registration_pool
