23f6b698c84cdda203992489be1a1449ce6485eb
Chris Dutra <cdutra@pivotal.io>
2017-06-16 12:19:50 -0700

    Remove unnecessary subnet from Azure terraform template
    [#134185071](https://www.pivotaltracker.com/story/show/134185071)
    
    Signed-off-by: Steve Weber <steveweb@microsoft.com>

--- azure/assets/terraform/terraform.tf
+++ azure/assets/terraform/terraform.tf
@@ -34,13 +34,6 @@ resource "azurerm_subnet" "Azure_bosh_subnet" {
   address_prefix       = "10.0.0.0/24"
 }
 
-resource "azurerm_subnet" "Azure_bosh_subnet_2" {
-  name                 = "Azure_bosh_subnet_2"
-  resource_group_name  = "${azurerm_resource_group.Azure_rg_bosh.name}"
-  virtual_network_name = "${azurerm_virtual_network.Azure_bosh_network.name}"
-  address_prefix       = "10.0.1.0/24"
-}
-
 resource "azurerm_subnet" "Azure_BATs_subnet" {
   name                 = "Azure_BATs_subnet"
   resource_group_name  = "${azurerm_resource_group.Azure_rg_bosh.name}"
