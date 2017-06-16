4613d5581cc5ec97a0ea5443a0c2bf3362c33452
Steve Weber <steveweb@microsoft.com>
2017-06-16 12:25:30 -0700

    Use lowercase resource names
    [#134185071](https://www.pivotaltracker.com/story/show/134185071)
    
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

--- azure/assets/terraform/terraform.tf
+++ azure/assets/terraform/terraform.tf
@@ -15,67 +15,66 @@ provider "azurerm" {
   tenant_id       = "${var.azure_tenant_id}"
 }
 # Create a resource group
-resource "azurerm_resource_group" "Azure_rg_bosh" {
+resource "azurerm_resource_group" "azure_rg_bosh" {
   name     = "${var.env_name}-rg"
   location = "${var.location}"
 }
-# Create a virtual network in the Azure_rg_bosh resource group
-resource "azurerm_virtual_network" "Azure_bosh_network" {
-  name                = "Azure_bosh_network"
+# Create a virtual network in the azure_rg_bosh resource group
+resource "azurerm_virtual_network" "azure_bosh_network" {
+  name                = "azure_bosh_network"
   address_space       = ["10.0.0.0/16"]
   location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.Azure_rg_bosh.name}"
+  resource_group_name = "${azurerm_resource_group.azure_rg_bosh.name}"
 }
 
-resource "azurerm_subnet" "Azure_bosh_subnet" {
-  name                 = "Azure_bosh_subnet"
-  resource_group_name  = "${azurerm_resource_group.Azure_rg_bosh.name}"
-  virtual_network_name = "${azurerm_virtual_network.Azure_bosh_network.name}"
+resource "azurerm_subnet" "azure_bosh_subnet" {
+  name                 = "azure_bosh_subnet"
+  resource_group_name  = "${azurerm_resource_group.azure_rg_bosh.name}"
+  virtual_network_name = "${azurerm_virtual_network.azure_bosh_network.name}"
   address_prefix       = "10.0.0.0/24"
 }
 
-resource "azurerm_subnet" "Azure_BATs_subnet" {
-  name                 = "Azure_BATs_subnet"
-  resource_group_name  = "${azurerm_resource_group.Azure_rg_bosh.name}"
-  virtual_network_name = "${azurerm_virtual_network.Azure_bosh_network.name}"
+resource "azurerm_subnet" "azure_bats_subnet" {
+  name                 = "azure_bats_subnet"
+  resource_group_name  = "${azurerm_resource_group.azure_rg_bosh.name}"
+  virtual_network_name = "${azurerm_virtual_network.azure_bosh_network.name}"
   address_prefix       = "10.0.16.0/24"
 }
 
-resource "azurerm_subnet" "Azure_BATs_subnet_2" {
-  name                 = "Azure_BATs_subnet_2"
-  resource_group_name  = "${azurerm_resource_group.Azure_rg_bosh.name}"
-  virtual_network_name = "${azurerm_virtual_network.Azure_bosh_network.name}"
+resource "azurerm_subnet" "azure_bats_subnet_2" {
+  name                 = "azure_bats_subnet_2"
+  resource_group_name  = "${azurerm_resource_group.azure_rg_bosh.name}"
+  virtual_network_name = "${azurerm_virtual_network.azure_bosh_network.name}"
   address_prefix       = "10.0.17.0/24"
 }
 
-# Create a Storage Account in the Azure_rg_bosh resouce group
-resource "azurerm_storage_account" "azureboshsa" {
+# Create a Storage Account in the azure_rg_bosh resouce group
+resource "azurerm_storage_account" "azure_bosh_sa" {
   name                = "${replace(var.env_name, "-", "")}"
-  resource_group_name = "${azurerm_resource_group.Azure_rg_bosh.name}"
+  resource_group_name = "${azurerm_resource_group.azure_rg_bosh.name}"
   location            = "${var.location}"
   account_type        = "Standard_LRS"
 }
 # Create a Storage Container for the bosh director
-resource "azurerm_storage_container" "azureboshcontainer" {
+resource "azurerm_storage_container" "azure_bosh_container" {
   name                  = "bosh"
-  resource_group_name   = "${azurerm_resource_group.Azure_rg_bosh.name}"
+  resource_group_name   = "${azurerm_resource_group.azure_rg_bosh.name}"
   storage_account_name  = "${azurerm_storage_account.azureboshsa.name}"
   container_access_type = "private"
 }
 # Create a Storage Container for the stemcells
-resource "azurerm_storage_container" "azurestemcontainer" {
+resource "azurerm_storage_container" "azure_stemcell_container" {
   name                  = "stemcell"
-  resource_group_name   = "${azurerm_resource_group.Azure_rg_bosh.name}"
+  resource_group_name   = "${azurerm_resource_group.azure_rg_bosh.name}"
   storage_account_name  = "${azurerm_storage_account.azureboshsa.name}"
   container_access_type = "blob"
 }
 
-
 # Create a Network Securtiy Group
-resource "azurerm_network_security_group" "Azure_bosh_nsg" {
-  name                = "Azure_bosh_nsg"
+resource "azurerm_network_security_group" "azure_bosh_nsg" {
+  name                = "azure_bosh_nsg"
   location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.Azure_rg_bosh.name}"
+  resource_group_name = "${azurerm_resource_group.azure_rg_bosh.name}"
 
   security_rule {
     name                       = "ssh"
@@ -127,69 +126,69 @@ resource "azurerm_network_security_group" "Azure_bosh_nsg" {
 }
 
 # Public IP Address for bosh
-resource "azurerm_public_ip" "Azure_IP_bosh" {
-  name                         = "Azure_IP_bosh"
+resource "azurerm_public_ip" "azure_ip_bosh" {
+  name                         = "azure_ip_bosh"
   location                     = "${var.location}"
-  resource_group_name          = "${azurerm_resource_group.Azure_rg_bosh.name}"
+  resource_group_name          = "${azurerm_resource_group.azure_rg_bosh.name}"
   public_ip_address_allocation = "static"
 }
 
 # Public IP Address for BATS
-resource "azurerm_public_ip" "Azure_IP_bats" {
-  name                         = "Azure_IP_bats"
+resource "azurerm_public_ip" "azure_ip_bats" {
+  name                         = "azure_ip_bats"
   location                     = "${var.location}"
-  resource_group_name          = "${azurerm_resource_group.Azure_rg_bosh.name}"
+  resource_group_name          = "${azurerm_resource_group.azure_rg_bosh.name}"
   public_ip_address_allocation = "static"
 }
 
 output "DirectorPublicIP" {
-  value = "${azurerm_public_ip.Azure_IP_bosh.ip_address}"
+  value = "${azurerm_public_ip.azure_ip_bosh.ip_address}"
 }
 output "Network" {
-  value = "${azurerm_virtual_network.Azure_bosh_network.name}"
+  value = "${azurerm_virtual_network.azure_bosh_network.name}"
 }
 output "Subnetwork" {
-  value = "${azurerm_subnet.Azure_bosh_subnet.name}"
+  value = "${azurerm_subnet.azure_bosh_subnet.name}"
 }
 output "ResourceGroupName" {
-  value = "${azurerm_resource_group.Azure_rg_bosh.name}"
+  value = "${azurerm_resource_group.azure_rg_bosh.name}"
 }
 output "StorageAccountName" {
   value = "${azurerm_storage_account.azureboshsa.name}"
 }
 output "DefaultSecurityGroup" {
-  value = "${azurerm_network_security_group.Azure_bosh_nsg.name}"
+  value = "${azurerm_network_security_group.azure_bosh_nsg.name}"
 }
 output "InternalCIDR" {
-  value = "${azurerm_subnet.Azure_bosh_subnet.address_prefix}"
+  value = "${azurerm_subnet.azure_bosh_subnet.address_prefix}"
 }
 output "InternalGateway" {
-  value = "${cidrhost(azurerm_subnet.Azure_bosh_subnet.address_prefix, 1)}"
+  value = "${cidrhost(azurerm_subnet.azure_bosh_subnet.address_prefix, 1)}"
 }
 output "ReservedRange" {
-  value = "${cidrhost(azurerm_subnet.Azure_bosh_subnet.address_prefix, 2)}-${cidrhost(azurerm_subnet.Azure_bosh_subnet.address_prefix, 6)}"
+  value = "${cidrhost(azurerm_subnet.azure_bosh_subnet.address_prefix, 2)}-${cidrhost(azurerm_subnet.azure_bosh_subnet.address_prefix, 6)}"
 }
 output "BATsPublicIP" {
-  value = "${azurerm_public_ip.Azure_IP_bats.ip_address}"
+  value = "${azurerm_public_ip.azure_ip_bats.ip_address}"
 }
 output "BATsNetwork" {
   value = {
-    Name = "${azurerm_subnet.Azure_BATs_subnet.name}"
-    CIDR = "${azurerm_subnet.Azure_BATs_subnet.address_prefix}"
-    Gateway = "${cidrhost(azurerm_subnet.Azure_BATs_subnet.address_prefix, 1)}"
-    ReservedRange = "${cidrhost(azurerm_subnet.Azure_BATs_subnet.address_prefix, 2)}-${cidrhost(azurerm_subnet.Azure_BATs_subnet.address_prefix, 3)}"
-    StaticRange =  "${cidrhost(azurerm_subnet.Azure_BATs_subnet.address_prefix, 4)}-${cidrhost(azurerm_subnet.Azure_BATs_subnet.address_prefix, 10)}"
-    StaticIP = "${cidrhost(azurerm_subnet.Azure_BATs_subnet.address_prefix, 4)}"
-    StaticIP_2 = "${cidrhost(azurerm_subnet.Azure_BATs_subnet.address_prefix, 5)}"
+    Name = "${azurerm_subnet.azure_bats_subnet.name}"
+    CIDR = "${azurerm_subnet.azure_bats_subnet.address_prefix}"
+    Gateway = "${cidrhost(azurerm_subnet.azure_bats_subnet.address_prefix, 1)}"
+    ReservedRange = "${cidrhost(azurerm_subnet.azure_bats_subnet.address_prefix, 2)}-${cidrhost(azurerm_subnet.azure_bats_subnet.address_prefix, 3)}"
+    StaticRange =  "${cidrhost(azurerm_subnet.azure_bats_subnet.address_prefix, 4)}-${cidrhost(azurerm_subnet.azure_bats_subnet.address_prefix, 10)}"
+    StaticIP = "${cidrhost(azurerm_subnet.azure_bats_subnet.address_prefix, 4)}"
+    StaticIP_2 = "${cidrhost(azurerm_subnet.azure_bats_subnet.address_prefix, 5)}"
   }
 }
 output "BATsSecondNetwork" {
   value = {
-    Name = "${azurerm_subnet.Azure_BATs_subnet_2.name}"
-    CIDR = "${azurerm_subnet.Azure_BATs_subnet_2.address_prefix}"
-    Gateway = "${cidrhost(azurerm_subnet.Azure_BATs_subnet_2.address_prefix, 1)}"
-    ReservedRange = "${cidrhost(azurerm_subnet.Azure_BATs_subnet_2.address_prefix, 2)}-${cidrhost(azurerm_subnet.Azure_BATs_subnet_2.address_prefix, 3)}"
-    StaticRange =  "${cidrhost(azurerm_subnet.Azure_BATs_subnet_2.address_prefix, 4)}-${cidrhost(azurerm_subnet.Azure_BATs_subnet_2.address_prefix, 10)}"
-    StaticIP = "${cidrhost(azurerm_subnet.Azure_BATs_subnet_2.address_prefix, 4)}"
+    Name = "${azurerm_subnet.azure_bats_subnet_2.name}"
+    CIDR = "${azurerm_subnet.azure_bats_subnet_2.address_prefix}"
+    Gateway = "${cidrhost(azurerm_subnet.azure_bats_subnet_2.address_prefix, 1)}"
+    ReservedRange = "${cidrhost(azurerm_subnet.azure_bats_subnet_2.address_prefix, 2)}-${cidrhost(azurerm_subnet.azure_bats_subnet_2.address_prefix, 3)}"
+    StaticRange =  "${cidrhost(azurerm_subnet.azure_bats_subnet_2.address_prefix, 4)}-${cidrhost(azurerm_subnet.azure_bats_subnet_2.address_prefix, 10)}"
+    StaticIP = "${cidrhost(azurerm_subnet.azure_bats_subnet_2.address_prefix, 4)}"
   }
 }
