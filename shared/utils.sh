0d14e716fd09969eafb05269da097db12e405ba9
Joe Baldwin <jbaldwin@pivotal.io>
2017-06-22 11:19:16 -0700

    Refactor shared/utils.sh to set $bosh_cli correctly for old style references
    [#147049339](https://www.pivotaltracker.com/story/show/147049339)
    
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

--- shared/utils.sh
+++ shared/utils.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-function setup_bosh_cli() {
-  bosh_cli=$(realpath bosh-cli/*bosh-cli-* 2>/dev/null)
-  if [[ $? -eq 0 ]]; then
-    chmod +x $bosh_cli
-    mv $bosh_cli /usr/local/bin/bosh2
-  fi
-}
-
-setup_bosh_cli
+# Oportunistically configure bosh2 for use
+local bosh_src="$(realpath bosh-cli/*bosh-cli-* 2>/dev/null)"
+if [[ $? -eq 0 ]]; then
+  export bosh_cli="/usr/local/bin/bosh2"
+  mv "${bosh_src}" "${bosh_cli}"
+  chmod +x "${bosh_cli}"
+  echo "bosh2 installed at ${bosh_cli}"
+else
+  echo "bosh2 not installed because bosh-cli input is not available"
+fi
