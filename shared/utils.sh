0c8f10792a614c85b59dfd0e798faa8e04a99d17
Joe Baldwin <jbaldwin@pivotal.io>
2017-06-22 14:10:54 -0700

    Refactor shared/utils.sh to work when set -e is in effect
    [#147049339](https://www.pivotaltracker.com/story/show/147049339)
    
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

--- shared/utils.sh
+++ shared/utils.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Oportunistically configure bosh2 for use
-bosh_src="$(realpath bosh-cli/*bosh-cli-* 2>/dev/null)"
-if [[ $? -eq 0 ]]; then
-  export bosh_cli="/usr/local/bin/bosh2"
-  mv "${bosh_src}" "${bosh_cli}"
-  chmod +x "${bosh_cli}"
-  echo "bosh2 installed at ${bosh_cli}"
-else
-  echo "bosh2 not installed because bosh-cli input is not available"
-fi
+configure_bosh_cli() {
+  local bosh_input="$(realpath bosh-cli/*bosh-cli-* 2>/dev/null || true)"
+  if [[ -n "${bosh_input}" ]]; then
+    export bosh_cli="/usr/local/bin/bosh2"
+    mv "${bosh_input}" "${bosh_cli}"
+    chmod +x "${bosh_cli}"
+  fi
+}
+configure_bosh_cli
