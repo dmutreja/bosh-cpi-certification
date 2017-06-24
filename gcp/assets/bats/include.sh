c32aa5f98d7ae6326ba56b1d57a713316c35c899
Chris Dutra <cdutra@pivotal.io>
2017-06-23 18:17:04 -0700

    consolidade run-bats

--- gcp/assets/bats/include.sh
+++ gcp/assets/bats/include.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
 
-function director_public_ip {
-  local metadata=${1:?}
-  echo ${metadata} | jq --raw-output ".external_ip"
-}
-
 function create_bats_env {
-  local metadata=${1:?}
-  local director_external_ip=$( echo ${metadata} | jq --raw-output ".external_ip" )
-
 cat <<EOF
 #!/usr/bin/env bash
-
-export BAT_INFRASTRUCTURE=gcp
-export BAT_NETWORKING=dynamic
-export BAT_RSPEC_FLAGS="--tag ~multiple_manual_networks --tag ~raw_ephemeral_storage --tag ~changing_static_ip"
 EOF
 }
