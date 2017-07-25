1b88e7f994bcd7e51f86e711bc4d7eed8baf2dbd
Chris Dutra <cdutra@pivotal.io>
2017-07-24 17:58:40 -0700

    Deprecate `bosh-certification-receipt-generator`
    - simple bash script generates receipt file
    
    [#149381875](https://www.pivotaltracker.com/story/show/149381875)
    
    Signed-off-by: Kaiting Chen <kaitingc@vmware.com>

--- shared/tasks/generate-receipt.sh
+++ shared/tasks/generate-receipt.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+source pipelines/shared/utils.sh
+
 : ${CPI_RELEASE_NAME:?}
 : ${STEMCELL_NAME:?}
 
@@ -12,8 +14,8 @@ STEMCELL_VERSION=$(cat stemcell/version)
 timestamp="$(date -u +%Y%m%d%H%M%S)"
 contents_hash=$(echo bosh-${BOSH_RELEASE_VERSION}-${CPI_RELEASE_NAME}-${CPI_RELEASE_VERSION}-${STEMCELL_NAME}-${STEMCELL_VERSION} | md5sum | cut -f1 -d ' ')
 
-certify-artifacts                                   \
-  --release bosh/$BOSH_RELEASE_VERSION              \
-  --release $CPI_RELEASE_NAME/$CPI_RELEASE_VERSION  \
-  --stemcell $STEMCELL_NAME/$STEMCELL_VERSION       \
+certify_artifacts \
+  bosh $BOSH_RELEASE_VERSION \
+  $CPI_RELEASE_NAME $CPI_RELEASE_VERSION \
+  $STEMCELL_NAME $STEMCELL_VERSION \
   > certification/${contents_hash}-${timestamp}-receipt.json
--- shared/utils.sh
+++ shared/utils.sh
@@ -13,3 +13,35 @@ configure_bosh_cli
 
 state_path() { bosh2 int director-state/director.yml --path="$1" ; }
 creds_path() { bosh2 int director-state/creds.yml --path="$1" ; }
+
+certify_artifacts() {
+  local usage="Usage: certify_artifacts BOSH_RELEASE_NAME BOSH_RELEASE_VERSION CPI_RELEASE_NAME CPI_RELEASE_VERSION STEMCELL_NAME STEMCELL_VERSION"
+
+  local bosh_release_name=${1?$usage}
+  local bosh_release_version=${2?$usage}
+
+  local cpi_release_name=${3?$usage}
+  local cpi_release_version=${4?$usage}
+
+  local stemcell_name=${5?$usage}
+  local stemcell_version=${6?$usage}
+
+  cat  <<EOF
+{
+  "releases": [
+    {
+      "name": "$bosh_release_name",
+      "version": "$bosh_release_version"
+    },
+    {
+      "name": "$cpi_release_name",
+      "version": "$cpi_release_version"
+    }
+  ],
+  "stemcell": {
+    "name": "$stemcell_name",
+    "version": "$stemcell_version"
+  }
+}
+EOF
+}
