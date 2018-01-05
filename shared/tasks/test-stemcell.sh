96581590991d466bb4c8d3379d92ddb3d8d97d7f
Chris Dutra <cdutra@pivotal.io>
2018-01-04 16:49:37 -0800

    Add update-config-config task to vsphere pipeline
    [#153031164](https://www.pivotaltracker.com/story/show/153031164)
    
    Signed-off-by: Luan Santos <lsantos@pivotal.io>

--- shared/tasks/test-stemcell.sh
+++ shared/tasks/test-stemcell.sh
@@ -9,6 +9,7 @@ creds_path /jumpbox_ssh/private_key > /tmp/jumpbox_private_key
 chmod 600 /tmp/jumpbox_private_key
 export BOSH_GW_PRIVATE_KEY=/tmp/jumpbox_private_key
 
+export BOSH_BINARY_PATH=$(which bosh)
 export SYSLOG_RELEASE_PATH=$(realpath syslog-release/*.tgz)
 export OS_CONF_RELEASE_PATH=$(realpath os-conf-release/*.tgz)
 export STEMCELL_PATH=$(realpath stemcell/*.tgz)
---
+++ shared/tasks/update-cloud-config.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eu
+
+: ${INFRASTRUCTURE:?}
+: ${DIRECTOR_VARS_FILE:?}
+
+source pipelines/shared/utils.sh
+source director-state/director.env
+
+bosh -n update-cloud-config pipelines/shared/assets/certification-release/cloud-config.yml \
+  -o pipelines/$INFRASTRUCTURE/assets/certification/cloud-config-ops.yml \
+  -l environment/metadata \
+  -l "$DIRECTOR_VARS_FILE" \
+  $( echo ${OPTIONAL_OPS_FILE} )
