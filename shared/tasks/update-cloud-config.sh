5816d8d2bd965ad3125a01f03697f64e5dd0384d
Difan Zhao <dzhao@pivotal.io>
2018-01-05 10:33:56 -0800

    Use director vars file path
    - fix aws test-stemcell
    
    [#153031164](https://www.pivotaltracker.com/story/show/153031164)
    
    Signed-off-by: Chris Dutra <cdutra@pivotal.io>

--- shared/tasks/update-cloud-config.sh
+++ shared/tasks/update-cloud-config.sh
@@ -11,5 +11,5 @@ source director-state/director.env
 bosh -n update-cloud-config pipelines/shared/assets/certification-release/cloud-config.yml \
   -o pipelines/$INFRASTRUCTURE/assets/certification/cloud-config-ops.yml \
   -l environment/metadata \
-  -l "$DIRECTOR_VARS_FILE" \
+  -l <( echo "$DIRECTOR_VARS_FILE" ) \
   $( echo ${OPTIONAL_OPS_FILE} )
